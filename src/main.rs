5e3aa1ab35847238ebfd6d60393462796077ac55
XauDe <xander.yuio@yahoo.com>
2023-08-26 11:44:36 +0100

    ..

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use crate::problem_two::{area, perimeter};
-
-mod problem_two;
+mod problem_one;
+use problem_one::p_1;
 
 fn main() {
-   
+   let a =4;
+   let b =5 ;
+   p_1(a, b);
 }
--- src/problem_one.rs
+++ src/problem_one.rs
@@ -5,7 +5,7 @@ the first problem requires you to make  a program that prints out the values of 
 pub fn p_1(x:i32 , y : i32){
     println!("A = x^2 + y^2 \nwhen x = {}, and y= {} \n A = {} ", x, y, x.pow(2) + y.pow(2));
 
-    println!("\n\nB = A^2 + xy \nwhen x = {}, and y= {} \n B = {} ", x, y, (x.pow(2) + y.pow(2))+ x*y);
+    println!("\n\nB = A^2 + xy \nwhen x = {}, and y= {} \n B = {} ", x, y, ((x.pow(2) + y.pow(2)).pow(2))+ x*y);
 
-    println!("\n\nC = (A+B)^2 \nwhen x = {}, and y= {} \n C = {} ", x, y, ((x.pow(2) + y.pow(2)) + ( (x.pow(2) + y.pow(2))+ x*y)).pow(2))
+    println!("\n\nC = (A+B)^2 \nwhen x = {}, and y= {} \n C = {} ", x, y, ( (x.pow(2) + y.pow(2))/*A*/ + (((x.pow(2) + y.pow(2)).pow(2))+ x*y)/* B */  ).pow(2)   )
 } 
